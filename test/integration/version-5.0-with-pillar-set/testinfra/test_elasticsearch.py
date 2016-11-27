e0a112537b0a317978d695638bec9472c2de7ee8
Michal Kuffa <michal.kuffa@gmail.com>
2016-11-27 23:55:23 +0100

    Test package version for 5. with pillar set
    Signed-off-by: Michal Kuffa <michal.kuffa@gmail.com>

--- test/integration/version-5.0-with-pillar-set/testinfra/test_elasticsearch.py
+++ test/integration/version-5.0-with-pillar-set/testinfra/test_elasticsearch.py
@@ -5,6 +5,12 @@ def test_kopf_plugin_is_installed(File):
     assert File('/usr/share/elasticsearch/plugins/kopf').exists
 
 
+def test_package_in_installed(Package):
+    elasticsearch = Package('elasticsearch')
+    assert elasticsearch.is_installed
+    assert elasticsearch.version.startswith('5.0')
+
+
 def test_service_is_running_and_enabled(Service):
     elasticsearch = Service('elasticsearch')
     assert elasticsearch.is_running
