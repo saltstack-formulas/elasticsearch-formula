a7731636e0e523f7e476e8a6503b0ee1d27755ea
Brandon Bradley <bradleytastic@gmail.com>
2017-01-26 20:57:33 -0600

    verbose output for script

--- test/run.sh
+++ test/run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+set -x
+
 rm -rf vendor
 mkdir vendor
 cd vendor
 git clone https://github.com/saltstack-formulas/sun-java-formula
 cd ..
-BUNDLE_PATH=vendor/bundle bundle exec kitchen verify
+bundle exec kitchen verify
